9bc32626f56c91d1c846efa8ec180754d913c781
cianorob <cianorob@goldendelicious.informatik.hs-furtwangen.de>
2013-10-17 15:21:26 +0200

    Initial Commit

---
+++ Vehicle/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Vehicle
+//
+//  Created by cianorob on 17.10.13.
+//  Copyright (c) 2013 cianorob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
